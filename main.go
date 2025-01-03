0aa7ab96e70ec6574dc41db6f14b7462d768b380
helmigandi <sugandihelmi@gmail.com>
2025-01-03 10:58:04 +0700

    Add REPL

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	// This blocks the code and waits for input, once the user types something and presses enter
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
 }
 
 // The purpose of this function will be to split the users input into "words" based on whitespace.
